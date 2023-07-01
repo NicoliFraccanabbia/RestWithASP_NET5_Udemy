ca38a0a797c3de6b6b409e77409d96c9f1bdcd08
Nicoli Rama Fraccanabbia <nicolirama@hotmail.com>
2023-07-01 19:16:30 -0300

    Definindo a interface

---
+++ RestWithASP_NET5-Udemy/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASP_NET5_Udemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASP_NET5_Udemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
